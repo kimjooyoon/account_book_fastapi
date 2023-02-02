43dca43849e7d872cca3dad71aab65d99ea796f0
kimjooyoon <asketeddy@gmail.com>
2023-02-02 20:04:53 +0900

    feat: db-middleware 설정 및 user_router 추가

--- core/api/main.py
+++ core/api/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi_sqlalchemy import DBSessionMiddleware
 
+from core.api.user.router import user_router
+from fastapi_sqlalchemy import db
+
+DB_URL = 'mysql+pymysql://root:test@localhost:3306/account_book_api?charset=utf8'
 app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=DB_URL)
 
 
 @app.get("/")
@@ -11,3 +19,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+app.include_router(user_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
