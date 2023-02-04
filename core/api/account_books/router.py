e69852a3106eea69e746f9e3f24f8597b50f0545
kimjooyoon <asketeddy@gmail.com>
2023-02-04 12:38:13 +0900

    feat(a_books): post /accounts, 이미 존재한다면 추가하지 않습니다.

--- core/api/account_books/router.py
+++ core/api/account_books/router.py
@@ -23,6 +23,16 @@ def create_account_books(user_id, dest_date, money):
     return m.id
 
 
+def exist_dest_books(dest_date, user_id):
+    m = AccountBook()
+    query = db.session.query(
+        AccountBook.id
+    ).where(AccountBook.dest_date == dest_date, AccountBook.user_id == user_id).first()
+    if query is None:
+        return False
+    return True
+
+
 @account_books_router.post("/accounts")
 async def accounts_create(
         money: int = 0,
@@ -31,7 +41,8 @@ async def accounts_create(
     if verify(token):
         user_id = decode(token).get('user_id')
         now = datetime.datetime.today().strftime("%Y-%m-%d")
-
+        if exist_dest_books(now, user_id):
+            return {"result": now+", 일자 가계부는 이미 추가 되었습니다."}
         try:
             id = create_account_books(user_id, now, money)
             return {id}
